e84039940e99b46d4101d8448c4f244814ed0398
DELL <shaimahalduais@gmail.com>
2023-08-02 15:20:38 +0300

    new update3

---
+++ app/src/main/java/com/example/na5/shaima.kt
@@ -0,0 +1,11 @@
+package com.example.na5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class shaima : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shaima)
+    }
+}
